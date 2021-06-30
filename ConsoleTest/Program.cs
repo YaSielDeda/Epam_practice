c419925ac0a3969fdfd1723f2b7a0c4d47018062
YaHochuPitsi <bereg6484@gmail.com>
2021-06-30 20:05:06 +0400

    DAO and change projects .NET versions

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\www\source\repos\EPAM_Practice.File_storage_and_sharing_system\Test folder 1";
+            string[] names = Directory.GetFiles(path);
+            //foreach (var name in names)
+            //{
+            //    FileInfo fi = new FileInfo(name);
+            //    Console.WriteLine($"{fi.Name}\t {fi.Length}\t {fi.CreationTime}\t {fi.LastWriteTime}");
+            //}
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+    }
+}
---
+++ EPAM.FileStorage.BLL/FileLogic.cs
@@ -0,0 +1,38 @@
+﻿using EPAM.FileStorage.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.FileStorage.BLL
+{
+    public class FileLogic
+    {
+        private FileDAO _dao;
+        public FileLogic(FileDAO dao)
+        {
+            _dao = dao;
+        }
+        public void CreateTextFile(string fileName)
+        {
+            _dao.CreateTextFile(fileName);
+        }
+        public void CreateTextFile(string fileName, string text)
+        {
+            _dao.CreateTextFile(fileName, text);
+        }
+        public void MoveFile(string destFilePath, string fileName)
+        {
+            _dao.MoveFile(destFilePath, fileName);
+        }
+        public void UpdateTextFile(string fileName, string text)
+        {
+            _dao.UpdateTextFile(fileName, text);
+        }
+        public void DeleteFile(string fileName)
+        {
+            _dao.DeleteFile(fileName);
+        }
+    }
+}
